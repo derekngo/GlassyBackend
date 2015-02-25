297deba18b53c999f0363cf044c93ed72209afa0
derekngo <derek.ngo1@yahoo.com>
2015-02-25 13:42:09 -0600

    serving static midi file

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -1,9 +1,24 @@
+import os
 import webapp2
+import jinja2
+from google.appengine.ext import db
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+		print("loading sound_test.html")
+		template= JINJA_ENVIRONMENT.get_template('sound_test.html')
+		self.response.write(template.render())
+		#self.response.headers['Content-Type'] = 'text/plain'
+        #self.response.write('Hello, World!')
+
+#class MidiFile(webapp2.RequestHandler):
+#	def get(self):
+#		return 
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
